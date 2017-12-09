cdd1b14fba9b43265f4338ada62a9e24e092b5fa
ismail Shuaau <ismailshuaau@gmail.com>
2017-12-09 13:43:02 +0500

    signup and login modal added

--- register.php
+++ register.php
@@ -5,7 +5,7 @@
 
   $account = new Account($pdo); // Created in to use the $account variable in register-handler.php amd register.php
 
-  function getInputValue($name){
+  function getInputValue($name) {
     if (isset($_POST[$name])) {
       echo $_POST[$name];
     }
@@ -14,6 +14,7 @@
   include("includes/handlers/register-handler.php");
   include("includes/handlers/login-handler.php");    
 ?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -23,10 +24,10 @@
     <meta name="author" content="">
     <link rel="icon" href="../../../../favicon.ico">
 
-    <title>Starter Template for Bootstrap</title>
+    <title>Dhivehi Podcast</title>
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <!-- Custom styles for this template -->
     <!-- <link href="starter-template.css" rel="stylesheet"> -->
     <link rel="stylesheet" href="assets/css/style.css">
@@ -51,100 +52,128 @@
           <li class="nav-item">
             <a class="nav-link disabled" href="#">Disabled</a>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown01">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+
+        <!-- Login button trigger modal -->
+        <button type="button" class="btn btn-link" data-toggle="modal" data-target="#loginModal">
+          Login
+        </button>  <!-- End- Login button trigger modal -->
+        <!-- Signup button trigger modal -->
+        <button type="button" class="btn btn-link" data-toggle="modal" data-target="#signupModal">
+          Sign up
+        </button> <!-- End - Signup button trigger modal -->
+
       </div>
     </nav>
-
+  
     <main role="main" class="container">
-      <div class="login-container">
 
-        <div class="input-container">
-          <div class="login-content">
-            <form id="loginForm" action="register.php" method="POST">
-              <h2>Got an account?</h2>  
-              <div class="form-group">
-                <?php echo $account->getError(Constants::$loginFailed); ?>
-                <label for="loginUsername">User</label>
-                <input type="email" class="form-control" id="loginUsername" name="loginUsername" type="text" placeholder="email" required>
-                <small id="emailHelp" class="form-text-muted">Your email is safe with us</small>
+      <div class="login">
+        <!-- Login Modal -->
+        <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="loginModalLabel">Login</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
               </div>
-              <div class="form-group">
-                <label for="loginPassword">Password</label>
-                <input type="password" class="form-control" id="loginPassword" name="loginPassword" placeholder="Password"  required>
-              </div>
-              <button type="submit" name="loginButton" class="btn btn-primary">LOG IN</button>
-            </form>
+              <form id="loginForm" action="register.php" method="POST">
+                <div class="modal-body">
+                  <h2>Got an account?</h2>  
+                  <div class="form-group">
+                    <?php echo $account->getError(Constants::$loginFailed); ?>
+                    <label for="loginUsername">User</label>
+                    <input type="email" class="form-control" id="loginUsername" name="loginUsername" type="text" placeholder="email" required>
+                    <small id="emailHelp" class="form-text-muted">Your email is safe with us</small>
+                  </div>
+                  <div class="form-group">
+                    <label for="loginPassword">Password</label>
+                    <input type="password" class="form-control" id="loginPassword" name="loginPassword" placeholder="Password"  required>
+                  </div>
+                </div> <!-- modal-body -->
+                <div class="modal-footer">
+                  <button type="submit" name="loginButton" class="btn btn-primary">LOG IN</button>
+                </div> <!-- modal-footer -->
+              </form>
+            </div>
           </div>
-    		  <hr>
-          <div class="login-content">
-        		<form id="registerForm" action="register.php" method="POST">
-        			<h2>Want a free account?</h2>
-        			<div class="form-group">
-                <?php echo $account->getError(Constants::$nickNameCharacters); ?>
-                <?php echo $account->getError(Constants::$userNameTaken); ?>
-        				<label for="nickname">Nick Name</label>
-        				<input type="text" class="form-control" id="nickname" name="nickname" placeholder="Nickname" value="<?php getInputValue('nickname') ?>" required>
-        			</div>
-        			<div class="form-group">
-                <?php echo $account->getError(Constants::$firstNameCharacters); ?>
-        				<label for="firstName">First Name</label>
-        				<input type="text" class="form-control" id="firstName" name="firstName" placeholder="First Name" value="<?php getInputValue('firstName') ?>" required>
-        			</div>
-        			<div class="form-group">
-                <?php echo $account->getError(Constants::$lastNameCharacters); ?>
-        				<label for="lastName">Last Name</label>
-        				<input type="text" class="form-control" id="lastName" name="lastName" placeholder="Last Name" value="<?php getInputValue('lastName') ?>" required>
-        			</div>
-        			<div class="form-group">
-                <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
-                <?php echo $account->getError(Constants::$emailInvalid); ?>
-        				<label for="email">Your Email</label>
-        				<input type="email" class="form-control" id="email" name="email" placeholder="Email" value="<?php getInputValue('email') ?>" required>
-        				<small id="emailHelp" class="form-text-muted">Your email is safe with us</small>
-        			</div>      			
-        			<div class="form-group">
-        				<label for="email2">Confirm Email</label>
-        				<input type="email" class="form-control" id="email2" name="email2" placeholder="Email" value="<?php getInputValue('email2') ?>" required>
-        				<small id="emailHelp" class="form-text-muted">Your email is safe with us</small>
-        			</div>
-        			<div class="form-group">
-                <?php echo $account->getError(Constants::$passwordsDoNotMatch); ?>
-                <?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
-                <?php echo $account->getError(Constants::$passwordCharacters); ?>
-                <?php echo $account->getError(Constants::$emailTaken); ?>
-        				<label for="password">Password</label>
-        				<input type="password" class="form-control" id="password" name="password" placeholder="Password"  required>
-        			</div>
-        			<div class="form-group">
-        				<label for="password2">Confirm Password</label>
-        				<input type="password" class="form-control" id="password2" name="password2" placeholder="Confirm Password"  required>
-        			</div>
-        			<button type="submit" name="registerButton" class="btn btn-primary">SIGN UP</button>
-        	 </form>
-         </div>
-	      </div> <!-- login-container -->
-      </div> <!-- login-content -->
+        </div>  <!-- End - Login Modal -->
+      </div>  <!-- login -->
 
-    </main><!-- /.container -->
+      <div class="signup">
+        <!-- Modal -->
+        <div class="modal fade" id="signupModal" tabindex="-1" role="dialog" aria-labelledby="signupModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="signupModalLabel">Sign up</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <form id="registerForm" action="register.php" method="POST">
+                <div class="modal-body">
+                    <h2>Want a free account?</h2>
+                    <div class="form-group">
+                      <?php echo $account->getError(Constants::$nickNameCharacters); ?>
+                      <?php echo $account->getError(Constants::$userNameTaken); ?>
+                      <label for="nickname">Nick Name</label>
+                      <input type="text" class="form-control" id="nickname" name="nickname" placeholder="Nickname" value="<?php getInputValue('nickname') ?>" required>
+                    </div>
+                    <div class="form-group">
+                    <?php echo $account->getError(Constants::$firstNameCharacters); ?>
+                      <label for="firstName">First Name</label>
+                      <input type="text" class="form-control" id="firstName" name="firstName" placeholder="First Name" value="<?php getInputValue('firstName') ?>" required>
+                    </div>
+                    <div class="form-group">
+                      <?php echo $account->getError(Constants::$lastNameCharacters); ?>
+                      <label for="lastName">Last Name</label>
+                       <input type="text" class="form-control" id="lastName" name="lastName" placeholder="Last Name" value="<?php getInputValue('lastName') ?>" required>
+                    </div>
+                    <div class="form-group">
+                      <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+                      <?php echo $account->getError(Constants::$emailInvalid); ?>
+                      <label for="email">Your Email</label>
+                      <input type="email" class="form-control" id="email" name="email" placeholder="Email" value="<?php getInputValue('email') ?>" required>
+                      <small id="emailHelp" class="form-text-muted">Your email is safe with us</small>
+                    </div>                 
+                    <div class="form-group">
+                      <label for="email2">Confirm Email</label>
+                      <input type="email" class="form-control" id="email2" name="email2" placeholder="Email" value="<?php getInputValue('email2') ?>" required>
+                      <small id="emailHelp" class="form-text-muted">Your email is safe with us</small>
+                    </div>
+                    <div class="form-group">
+                      <?php echo $account->getError(Constants::$passwordsDoNotMatch); ?>
+                      <?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
+                      <?php echo $account->getError(Constants::$passwordCharacters); ?>
+                      <?php echo $account->getError(Constants::$emailTaken); ?>
+                      <label for="password">Password</label>
+                      <input type="password" class="form-control" id="password" name="password" placeholder="Password"  required>
+                    </div>
+                    <div class="form-group">
+                      <label for="password2">Confirm Password</label>
+                      <input type="password" class="form-control" id="password2" name="password2" placeholder="Confirm Password"  required>
+                    </div>
+                </div> <!-- modal-body -->
+                <div class="modal-footer">
+                  <button type="submit" name="registerButton" class="btn btn-primary">SIGN UP</button>
+                </div> <!-- modal-footer -->
+              </form> <!-- registerForm -->
+            </div> <!-- modal-content -->
+          </div> <!-- modal-dialog -->
+        </div> <!-- Modal -->
+      </div> <!-- Signup -->
+
+    </main> <!-- /.container -->
 
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script src="assets/js/jquery-3.2.1.slim.min.js"></script>
+  	<script src="assets/js/popper.min.js"></script>
+  	<script src="assets/js/bootstrap.min.js"></script>
   </body>
 </html>
