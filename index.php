c5b7c2c6b6b638a4e35d4babeef2a5f5c39467dd
ismail Shuaau <ismailshuaau@gmail.com>
2017-12-11 07:41:09 +0500

    progess bar

--- index.php
+++ index.php
@@ -33,18 +33,25 @@ include("includes/config.php");
 				</div>
 				<div class="now-playing-center">
 					<div class="content player-controls">
-						<button class="fa fa-random" aria-hidden="true" title="shuffle" alt="shuffle"></button>
-						<button class="fa fa-caret-left" aria-hidden="true" title="previouse" alt="previouse"></button>
-						<button class="fa fa-play-circle" aria-hidden="true" title="play" alt="play"></button>
-						<button class="fa fa-pause-circle-o" aria-hidden="true" title="pause" alt="pause"></button>
-						<button class="fa fa-caret-right" aria-hidden="true" title="next" alt="next"></button>
-						<button class="fa fa fa-repeat" aria-hidden="true" title="repeat" alt="repeat"></button>
+						<div class="buttons">
+							<button class="control-button fa fa-random" aria-hidden="true" title="shuffle" alt="shuffle"></button>
+							<button class="control-button fa fa-caret-left" aria-hidden="true" title="previouse" alt="previouse"></button>
+							<button class="control-button fa fa-play-circle" aria-hidden="true" title="play" alt="play"></button>
+							<button class="control-button fa fa-pause-circle" aria-hidden="true" title="pause" alt="pause"></button>
+							<button class="control-button fa fa-caret-right" aria-hidden="true" title="next" alt="next"></button>
+							<button class="control-button fa fa fa-repeat" aria-hidden="true" title="repeat" alt="repeat"></button>
+						</div> <!-- buttons -->
+						<div class="play-back-bar">
+							<span class="progress-time current">0.00</span>
+							<div class="pogress-bar"></div>
+							<span class="progress-time remaining">0.00</span>	
+						</div>
 					</div>
 				</div>
 				<div class="now-playing-right">
 					
 				</div>
-			</div>
+			</div> <!-- now-playing-bar -->
 		</div>
 	</body>
 
