2383c963e94698067e79229725e028c07a7eb543
ismail Shuaau <ismailshuaau@gmail.com>
2018-01-03 17:51:13 +0500

    playlist class

---
+++ includes/classes/Playlist.php
@@ -0,0 +1,30 @@
+<?php 
+	class Playlist {
+
+		private $pdo;
+		private $id;
+		private $name;
+		private $owner;
+
+		public function __construct($pdo, $data) {
+			$this->pdo = $pdo;
+			$this->id = $data['id'];
+			$this->name = $data['name'];
+			$this->owner = $data['owner'];
+		}
+
+		public function getId() {
+			return $this->id;
+		}
+
+		public function getName() {
+			return $this->name;
+		}
+
+		public function getOwner() {
+			return $this->owner;
+		}
+		
+	}
+
+ ?>
--- playlists.php
+++ playlists.php
@@ -14,6 +14,7 @@
 	 		<div>
 	 			<div class="row">
 					<?php
+
 						$username = $userLoggedIn->getUserName();
 						$playListQuery = ("SELECT * FROM playlists WHERE owner='$username'");
 						$playListQueries = $pdo->prepare($playListQuery, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
@@ -24,13 +25,15 @@
 							echo "<span class='no-results'> No playlist created </span>";
 						}
 
-						foreach ($playListQueries as $playlist) {
+						foreach ($playListQueries as $row) {
+
+							$playlist = new Playlist($pdo, $row);
 							echo "<div class='card-view'>
 									<div class='playlist-icon'>
 										<i class='fa fa-headphones' aria-hidden='true'></i>
 									</div>
 									<div class='playlist-caption'>"
-										. $playlist['name'] .
+										. $playlist->getName() .
 									"</div>
 								 </div>";
 						}
