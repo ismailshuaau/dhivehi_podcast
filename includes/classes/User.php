95862a70ce894ac55d01d1ff12d42ed84c7c04b1
ismail Shuaau <ismailshuaau@gmail.com>
2018-01-11 10:01:51 +0500

    User details form added

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -14,14 +14,25 @@
 			return $this->username;
 		}
 
+		public function getEmail() {
+			$query = ("SELECT email FROM users WHERE email='$this->username'");
+			$name = $this->pdo->prepare($query);
+			$name->execute();
+			$results = $name->fetchAll(PDO::FETCH_ASSOC);
+
+			return $results[0]['email'];
+		}
+
 		public function getFirstAndLastName(){
-			$query = ("SELECT CONCAT (firstName, ' ', lastName) as 'name' from users where email='$this->username'");
+			$query = ("SELECT CONCAT (firstName, ' ', lastName) AS 'name' FROM users WHERE email='$this->username'");
 			$name = $this->pdo->prepare($query);
 			$name->execute();
 			$results = $name->fetchAll(PDO::FETCH_ASSOC);
 
 			return $results[0]['name'];
 		}
+
+
 	}
 
 
--- updateDetails.php
+++ updateDetails.php
@@ -1,1 +1,22 @@
-HELLO!
+<?php 
+
+	include("includes/includedFiles.php");
+
+ ?>
+
+ <div class="user-details">
+ 	<div class="container border-bottom">
+ 		<h2>Email</h2>
+ 		<input type="text" class="email" name="email" placeholder="Email address..." value=" <?php echo $userLoggedIn->getEmail();	?>">
+ 		<span class="message"></span>
+ 		<button class="btn" onclick="">SAVE</button>
+ 	</div>
+ 	<div class="container">
+ 		<h3>Password</h3>
+ 		<input type="password" class="old-password" name="old-password" placeholder="Current Password">
+ 		<input type="password" class="new-password-1" name="new-password-1" placeholder="New Password">
+ 		<input type="password" class="new-password-2" name="new-password-2" placeholder="Confirm Password">
+ 		<span class="message"></span>
+		<button class="btn" onclick="">SAVE</button>
+ 	</div>
+ </div>
